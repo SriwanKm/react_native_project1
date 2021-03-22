191be9b60d8060af82121de5c15e76b36fb9691c
SriwanKm <kcqkoo@gmail.com>
2021-03-22 15:18:26 -0700

    add sub-header

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-      <Text style={styles.boldText}>Best Burger</Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.boldText}>Best Burger</Text>
+            </View>
+          <View style={styles.header}>
+            <Text style={styles.boldText}>burger</Text>
+          </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        backgroundColor: 'pink',
+        padding: 20,
+    },
+    boldText: {
+        fontWeight: 'bold',
+    }
 });
